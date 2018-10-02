23ead2d581284c878eb40135d904cc26a0be7ed1
derkalaender <derkalaender@gmx.de>
2018-10-02 20:58:00 +0200

    Added buttons for each extension
    No more hard-coded values

--- client/index.js
+++ client/index.js
@@ -1,10 +1,33 @@
 // Loading CS Interface and express via npm
-const csInterface = new CSInterface();
+const cs = new CSInterface();
+
+const loc = window.location.pathname;
+const dir = decodeURI(loc.substring(1, loc.lastIndexOf('/')));
+const extensions = require(dir + "/../reloadable.json");
 
 const http = new XMLHttpRequest();
-const url = "http://localhost:8081/kill";
 
-function reloadExtension() {
+function createButtons() {
+    Object.keys(extensions).forEach(function(key) {
+        const displayName = extensions[key]["displayName"];
+        const port = extensions[key]["port"];
+        const path = extensions[key]["path"];
+
+        const btn = document.createElement('button');
+        const text = document.createTextNode(displayName);
+        btn.appendChild(text);
+
+        btn.addEventListener('click', function () {
+            reloadExtension(key, port, path);
+        });
+
+        document.getElementById("extensions").appendChild(btn);
+    })
+}
+
+function reloadExtension(id, port, path) {
+    const url = "http://localhost:" + port + path;
+
     console.log('Reloading Extension');
 
     http.open("GET", url);
@@ -13,19 +36,18 @@ function reloadExtension() {
 
     http.onload = function () {
         console.log("Extension successfully killed");
-        requestExtension();
+        requestExtension(id);
     };
 
     http.onerror = function () {
         console.log("Extensions seems to be killed already. Calling it nevertheless.");
-        requestExtension();
+        requestExtension(id);
     }
 }
 
-function requestExtension() {
+function requestExtension(id) {
     //Wait half a second before launching again, otherwise Premiere just doesn't care
-
     setTimeout(function() {
-        csInterface.requestOpenExtension("de.sebinside.premiereahk.panel", "");
+        cs.requestOpenExtension(id, "");
     }, 500);
 }
